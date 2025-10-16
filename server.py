a6911cc8a296fd09049260e2f5f623cdfbb0b32d
s-cavalier <jrc974@ucsb.edu>
2025-10-16 00:39:18 -0700

    more api endpoints

--- server.py
+++ server.py
@@ -14,9 +14,52 @@ def home():
 
 # Place endpoints in this space; they define functions/routes the app will save and look out for when requests come in
 # It'll parse and match accordingly; otherwise, it'll reject with a 404 error
+# Also, you may see HTTP requests with an OPTIONS method; this just a "preflight" check done automatically to prevent against XSS (as mentioned above)
+
+database = []
+
+@app.get("/get-test")
+def get_test():
+    return "Hello"
+
+@app.post("/post-test")
+def post_test():
+    body = request.get_data(as_text=True)
+    
+    if body == "mail":
+        return "good"
+
+    return "bad"
+
+@app.put("/item")
+def put_test():
+    body = request.get_data(as_text=True)
+    database.append(body)
+    
+    return "All good"
+
+@app.get("/item/<name>")
+def get_item(name: str):
+    try:
+        ind = database.index(name)  
+        return f"Index is: {ind}"
+    except ValueError:
+        pass
+    
+    return "Couldn't find"
+
+@app.delete("/kill/<killed>")
+def delete_test(killed: str):
+    try:
+        database.remove(killed)
+        return "Killed it"
+    except ValueError:
+        pass
+    
+    return "Couldn't find anything"
 
 if __name__ == "__main__":
-    # Change port if necessary
+    # Change port if necessary (i.e., 8080 is occupied)
     app.run(host="localhost", port=8080)
 
 
